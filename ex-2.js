cebebb18d052f22e80152501198216df95324316
Apirak Fakin <apirakfakin@gmail.com>
2024-05-15 13:31:32 +0700

    feat: add ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,16 @@
 // Exercise #2: Loose Equality
 // Start commenting here
 0 == false;
+// true
+
 null == undefined;
+// true
+
 10 == "10";
+// true
+
 [10] == 10;
+// true
+
 [1,2] == "1,2";
+// true
