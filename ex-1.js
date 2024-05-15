8e530aec13eaebb9bf2dc0d07ee8d047586b69df
Puttapong <puttapong.lekk@gmail.com>
2024-05-15 13:30:33 +0700

    add ex1-ex2

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,19 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
-Object.is(0, -0);
-Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
-Object.is(null, undefined);
+Object.is(10, 20); //fale
+Object.is(0, -0); //fale
+Object.is(NaN, 100); //fale
+Object.is("one", "two"); //fale
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //fale
+Object.is(null, undefined); //fale
 
-10 === 20;
-10 === "10"
+10 === 20; //fale
+10 === "10"; //fale
 3.14 !== 3.14;
-"TechUp" !== "TechUp";
-NaN === NaN;
-0 !== -0;
+"TechUp" !== "TechUp"; //fale
+NaN === NaN; //fale
+0 !== -0; //fale
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;
+objectA === objectB; //fale
